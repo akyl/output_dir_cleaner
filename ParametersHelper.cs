42aa6fabb71b51ba9395e9a609cca95b8394efa8
Akyl Zholonov <Akyl_Zholonov@epam.com>
2022-12-09 14:56:37 +0600

    Initial commit

---
+++ ParametersHelper.cs
@@ -0,0 +1,23 @@
+﻿using Utility.CommandLine;
+
+namespace cleaner
+{
+	public static class ParametersHelper
+	{
+		public static bool? ParseStringToNullableBool(string input)
+		{
+			return input == null ? null : bool.Parse(input);
+		}
+
+		public static void ShowHelpAttributes(IEnumerable<ArgumentInfo> arguments)
+		{
+			Console.WriteLine("Long name | Description");
+			Console.WriteLine("---------------------------------------------");
+			foreach (var argument in arguments)
+			{
+				Console.WriteLine($"{argument.LongName} | {argument.HelpText}");
+			}
+		}
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Utility.CommandLine;
+
+namespace cleaner
+{
+    public class Program
+    {
+
+        private static string[] folderNames = new[] { "bin", "obj" };
+
+        [Argument('h', "Help", "Show all parameters")]
+        private static string Help { get; set; }
+
+        [Argument('C', "Clean", "Run clean tool")]
+        private static string Clean { get; set; }
+
+        public static void Main()
+        {
+            Console.WriteLine("Output folders clean tool");
+            Arguments.Populate();
+
+            if (Help == null && Clean == null)
+            {
+                Console.WriteLine("Please, use --Help command to see usage");
+                return;
+            }
+
+            if (Help != null)
+            {
+                var helpAttributes = Arguments.GetArgumentInfo(typeof(Program));
+                ParametersHelper.ShowHelpAttributes(helpAttributes);
+                return;
+            }
+
+            if (Clean == null)
+            {
+                Console.WriteLine("Please, use --Help command to see usage");
+                return;
+            }
+
+            Console.WriteLine($"Root foldert to clean is  {Clean}");
+            CleanFolder(Clean);
+
+            Console.WriteLine("\nCleaning finished. Press Enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void CleanFolder(string path)
+        {
+            var directories = Directory.GetDirectories(path);
+            foreach (var dir in directories)
+            {
+                if (folderNames.Contains(dir.Split('\\').Last()))
+                {
+                    Console.WriteLine($"Folder {dir} cleaned");
+                    Directory.Delete(dir, true);
+                }
+                else
+                {
+                    CleanFolder(dir);
+                }
+            }
+        }
+    }
+}
